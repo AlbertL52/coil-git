f7b7ffad60e864d1d5b3154934371ed71f7fed36
bT_1N3_08 <aliu0210@bths.edu>
2025-11-21 10:11:04 -0500

    coil-2

--- src/coil.java
+++ src/coil.java
@@ -1,5 +1,7 @@
 public class coil {
     public static void main(String[] args) {
-        System.out.println("Coil");
+        System.out.println("Coil is");
+        System.out.println("Better than boil");
+        int x = 1;
     }
 }
