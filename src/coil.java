b67da9e2025d8146c55de1289c1be02f07ec6d5c
bT_1N3_08 <aliu0210@bths.edu>
2025-11-21 10:22:42 -0500

    Foil a Coil

--- src/coil.java
+++ src/coil.java
@@ -2,8 +2,8 @@ public class coil {
     public static void main(String[] args) {
         System.out.println("Coil is");
         System.out.println("Better than boil");
-        int x = 0;
-        int boil = 1;
+        int x = -1;
+        int boil = 0;
         int coil = 2;
         System.out.println("coil better: " + (coil > boil));
         x = 2;
---
+++ src/foil.java
@@ -0,0 +1,9 @@
+public class foil {
+    public static void main(String[] args) {
+        int int1 = 0;
+        String str = "onetwo";
+        if (str.contains("two")){
+            int1 = str.indexOf("two");
+        }
+    }
+}
